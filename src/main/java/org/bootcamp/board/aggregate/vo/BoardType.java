d6b2d82993b81ea0e6ba3f28cc9b57b0e13fbe2d
Ssujimu <okq0223@naver.com>
2023-01-26 10:24:57 +0900

    bootcamp BoardType

--- src/main/java/org/bootcamp/board/aggregate/vo/BoardType.java
+++ src/main/java/org/bootcamp/board/aggregate/vo/BoardType.java
@@ -2,7 +2,7 @@ package org.bootcamp.board.aggregate.vo;
 
 public enum BoardType {
     Normal,                         // 일반
-    Qna,                            // 문의방
-    OneByOne,                       // 1대1
-    Tendinous,                      // 건의방
+    Notice,                         // 공지사항
+    FAQ,                            // FAQ
+    QNA,                            // QNA
 }
